744a011e9f667a8c5a43161e087035a525f69c37
Elliot Glaze <elliot.glaze@ITs-MacBook-Pro.local>
2023-04-27 15:56:45 +0100

    init SST commit

---
+++ stacks/ProfileStack.ts
@@ -0,0 +1,25 @@
+import { StackContext, Api, Cognito } from "sst/constructs";
+
+export function ProfileStack({ stack }: StackContext) {
+  const api = new Api(stack, "api", {
+    defaults: {
+      authorizer: "iam",
+    },
+    routes: {
+      "GET /": "packages/functions/src/lambda.handler",
+    },
+  });
+
+  const auth = new Cognito(stack, "auth", {
+    login: ["email"]
+  });
+
+  auth.attachPermissionsForAuthUsers(stack, [api]);
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    UserPoolId: auth.userPoolId,
+    IdentityPoolId: auth.cognitoIdentityPoolId,
+    UserPoolClientId: auth.userPoolClientId,
+  });
+}
